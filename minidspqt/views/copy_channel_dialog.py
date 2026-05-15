612b30d8a367b229a91e12d47e0ad4bd832fbfbb
Maximilian Zettler <info@max-zettler.de>
2026-05-15 10:57:38 +0200

    feat: add select all checkbox for parameter selection
    - Add tri-state 'Select all' checkbox in parameter header
    - Click to select all enabled params, click again to deselect all
    - Shows partial state when only some params are checked
    - Respects disabled state for slave-linked restrictions

--- minidspqt/views/copy_channel_dialog.py
+++ minidspqt/views/copy_channel_dialog.py
@@ -1,5 +1,6 @@
 """Dialog for copying channel settings from one channel to others."""
 
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QCheckBox,
     QComboBox,
@@ -60,7 +61,15 @@ class CopyChannelDialog(QDialog):
         source_layout.addStretch(1)
         root.addLayout(source_layout)
 
-        root.addWidget(QLabel("Parameters to copy:"))
+        root.addSpacing(8)
+        param_header = QHBoxLayout()
+        param_header.addWidget(QLabel("Parameters to copy:"))
+        param_header.addStretch(1)
+        self._select_all_cb = QCheckBox("Select all")
+        self._select_all_cb.setTristate(True)
+        self._select_all_cb.stateChanged.connect(self._on_select_all_toggled)
+        param_header.addWidget(self._select_all_cb)
+        root.addLayout(param_header)
         self._param_container = QGridLayout()
         root.addLayout(self._param_container)
 
@@ -134,12 +143,15 @@ class CopyChannelDialog(QDialog):
         for group in params:
             cb = QCheckBox(group)
             cb.setChecked(True)
+            cb.stateChanged.connect(self._on_param_toggled)
             self._param_checkboxes[group] = cb
             self._param_container.addWidget(cb, row, col)
             col += 1
             if col == 3:
                 row += 1
                 col = 0
+
+        self._sync_select_all()
 
         self._clear_layout(self._target_container)
         self._target_checkboxes.clear()
@@ -174,6 +186,8 @@ class CopyChannelDialog(QDialog):
             else:
                 cb.setEnabled(True)
 
+        self._sync_select_all()
+
         if any_slave and targets:
             names = [
                 self._display_name(t)
@@ -202,3 +216,49 @@ class CopyChannelDialog(QDialog):
 
         self.result_data = (source, targets, groups)
         self.accept()
+
+    def _on_select_all_toggled(self, state: int) -> None:
+        """Handle select-all checkbox state changes.
+
+        - Checked or PartiallyChecked: check all enabled param checkboxes
+        - Unchecked: uncheck all param checkboxes
+        """
+        enabled_checks = [cb for cb in self._param_checkboxes.values() if cb.isEnabled()]
+        if not enabled_checks:
+            return
+
+        if state == Qt.CheckState.Unchecked.value:
+            for cb in enabled_checks:
+                cb.setChecked(False)
+        else:
+            for cb in enabled_checks:
+                cb.setChecked(True)
+
+    def _on_param_toggled(self) -> None:
+        """Sync select-all checkbox when individual checkboxes change."""
+        self._sync_select_all()
+
+    def _sync_select_all(self) -> None:
+        """Update select-all checkbox based on individual checkbox states.
+
+        Shows:
+        - Checked: if all enabled checkboxes are checked
+        - Partially checked: if some (but not all) enabled checkboxes are checked
+        - Unchecked: if no enabled checkboxes are checked
+        """
+        enabled_checks = [cb for cb in self._param_checkboxes.values() if cb.isEnabled()]
+        if not enabled_checks:
+            self._select_all_cb.setCheckState(Qt.CheckState.Unchecked)
+            return
+
+        all_checked = all(cb.isChecked() for cb in enabled_checks)
+        any_checked = any(cb.isChecked() for cb in enabled_checks)
+
+        self._select_all_cb.blockSignals(True)
+        if all_checked:
+            self._select_all_cb.setCheckState(Qt.CheckState.Checked)
+        elif any_checked:
+            self._select_all_cb.setCheckState(Qt.CheckState.PartiallyChecked)
+        else:
+            self._select_all_cb.setCheckState(Qt.CheckState.Unchecked)
+        self._select_all_cb.blockSignals(False)
